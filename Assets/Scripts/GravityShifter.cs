c155720df3bfa68e6dea0ad3f28037e2861cd4f0
Marccio Silva <marcciosilva@gmail.com>
2017-03-18 00:05:55 -0300

    Add paused functionality in-game.

--- Assets/Scripts/GravityShifter.cs
+++ Assets/Scripts/GravityShifter.cs
@@ -8,15 +8,17 @@ public class GravityShifter : MonoBehaviour {
     private Rigidbody2D _physicsBody;
     public GameObject gravityParticleSystem;
     private GameObject _currentGravityParticleSystem;
+    private LevelManager _levelManager;
 
     // Use this for initialization
     void Start () {
         _physicsBody = GetComponent<Rigidbody2D>();
+        _levelManager = GameObject.Find("LevelManager").GetComponent <LevelManager> ();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Invert Gravity"))
+        if (!_levelManager.isGamePaused && Input.GetButtonDown("Invert Gravity"))
         {
             // Invert game object's gravity scale.
             _physicsBody.gravityScale *= -1.0f;
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
     private const string _loseSceneName = "Lose";
-    private float _levelDuration = 5;
+    private float _levelDuration = 30; // seconds
     private Text _timerText;
-
+    public bool isGamePaused = false;
+    private GameObject _canvasPaused;
 
 	// Use this for initialization
 	void Start () {
@@ -20,13 +21,43 @@ public class LevelManager : MonoBehaviour {
         {
             this.enabled = false;
         }
-
+        tmp = GameObject.Find("CanvasPaused");
+        if (tmp != null)
+        {
+            _canvasPaused = tmp;
+            _canvasPaused.SetActive(isGamePaused);
+        }
+        else
+        {
+            this.enabled = false;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        _timerText.text = "TIME LEFT - " + string.Format("{0}:{1:00}", (int)_levelDuration / 60, (int)_levelDuration % 60);
-        _levelDuration -= Time.deltaTime;
-        if (_levelDuration <= 1) SceneManager.LoadScene(_loseSceneName);
+        if (Input.GetButtonDown("Start"))
+        {
+            isGamePaused = !isGamePaused;
+            if (isGamePaused)
+            {
+                Time.timeScale = 0;
+                _canvasPaused.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                _canvasPaused.SetActive(false);
+            }
+            
+        }
+
+        if (!isGamePaused)
+        {
+            _timerText.text = "TIME LEFT - " + string.Format("{0}:{1:00}", (int)_levelDuration / 60, (int)_levelDuration % 60);
+            _levelDuration -= Time.deltaTime;
+            if (_levelDuration <= 1) SceneManager.LoadScene(_loseSceneName);
+        }
+
+
     }
 }
