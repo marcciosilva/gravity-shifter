66230876037aa6ae22428be8d7b4d0b78e49a505
Marccio Silva <marcciosilva@gmail.com>
2017-03-23 01:33:41 -0300

    Add moving platform and level 5.

---
+++ Assets/AutoMoveBetweenTwoPoints.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveBetweenTwoPoints : MonoBehaviour
+{
+
+    public Vector3 leftmostPosition;
+    // Configurable values.
+    public Vector3 rightmostPosition;
+    public bool moveOnX;
+    public bool moveOnY;
+    public float velocityOnX;
+    public float velocityOnY;
+    private bool initialMovement = true;
+    private Vector3 _startPosition;
+    private Vector2 _startPositionWithOffset;
+
+
+    void Start()
+    {
+        _startPosition = transform.position;
+        _startPositionWithOffset.x = _startPosition.x - leftmostPosition.x;
+        _startPositionWithOffset.y = _startPosition.y - leftmostPosition.y;
+    }
+
+    void Update()
+    {
+        if (moveOnX)
+        {
+            float newXPosition = _startPositionWithOffset.x + Time.time * velocityOnX;
+            transform.position = new Vector3(leftmostPosition.x + Mathf.PingPong(newXPosition, rightmostPosition.x), transform.position.y, transform.position.z);
+        }
+        if (moveOnY)
+        {
+            float newYPosition = _startPositionWithOffset.y + Time.time * velocityOnY;
+            transform.position = new Vector3(transform.position.x, leftmostPosition.y + Mathf.PingPong(newYPosition, rightmostPosition.y), transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Make player stay on platform.
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+
+}
