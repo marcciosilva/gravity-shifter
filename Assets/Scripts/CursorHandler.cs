552e09235b2e13918ca1e8c5420c7634a3a3dd34
Marccio Silva <marcciosilva@gmail.com>
2017-03-26 01:21:44 -0300

    Add same music to all menu levels.

--- Assets/Scripts/CursorHandler.cs
+++ Assets/Scripts/CursorHandler.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,22 +6,33 @@ public class CursorHandler : MonoBehaviour
 {
 
     private int _currentPosition = 0;
-    private int _availablePositions = 2;
+    private int _availablePositions = 3;
     private float _exitTextVertPosition;
     private float _startTextVertPosition;
     private float _delayBetweenActions = 0.5f; // seconds
     private float _timeSinceLastAction; // seconds
-
+    private float _secondOptionPosition;
+    private string _sceneName;
 
     // Use this for initialization
     void Start()
     {
+        // TODO should decouple this to make it reusable for any type of menu.
+        _sceneName = SceneManager.GetActiveScene().name;
         GameObject tmp = GameObject.Find("TextStartGame");
         if (tmp != null)
             _startTextVertPosition = tmp.transform.position.y;
         tmp = GameObject.Find("TextExitGame");
         if (tmp != null)
             _exitTextVertPosition = tmp.transform.position.y;
+        if (_sceneName == "MainMenu")
+        {
+            tmp = GameObject.Find("TextCredits");
+        } else if (_sceneName == "Lose")
+        {
+            tmp = GameObject.Find("TextMainMenu");
+        }
+        _secondOptionPosition = tmp.transform.position.y;
     }
 
     // Update is called once per frame
@@ -39,15 +48,17 @@ public class CursorHandler : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
+            StartCoroutine(Fade());
             switch (_currentPosition)
             {
                 case 0:
-                    StartCoroutine(Fade());
-                    if (EditorSceneManager.GetActiveScene().name == "MainMenu") SceneManager.LoadScene("Tutorial");
+                    if (_sceneName == "MainMenu") SceneManager.LoadScene("Tutorial");
                     else SceneManager.LoadScene("Level-1");
                     break;
                 case 1:
-                    StartCoroutine(Fade());
+                    SceneManager.LoadScene("Credits");
+                    break;
+                case 2:
                     Application.Quit();
                     break;
                 default:
@@ -86,6 +97,9 @@ public class CursorHandler : MonoBehaviour
                         transform.position = new Vector3(transform.position.x, _startTextVertPosition, transform.position.z);
                         break;
                     case 1:
+                        transform.position = new Vector3(transform.position.x, _secondOptionPosition, transform.position.z);
+                        break;
+                    case 2:
                         transform.position = new Vector3(transform.position.x, _exitTextVertPosition, transform.position.z);
                         break;
                     default:
--- Assets/Scripts/InteractiveText.cs
+++ Assets/Scripts/InteractiveText.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -51,7 +50,7 @@ public class InteractiveText : MonoBehaviour
         while (i < strComplete.Length)
         {
             _text.text += strComplete[i++];
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.01f);
         }
         _busy = false;
     }
